ba7fcd314d179c9f3a48a5c5b0a63433ff79d9f0
Minh-EPFL <minh1093@gmail.com>
2015-03-08 11:15:24 +0100

    Récupération de la ligne.

--- sim.c
+++ sim.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "sim.h"
 #include "particule.h"
 #include "trounoir.h"
@@ -5,10 +8,17 @@
 
 void sim_lecture(char* nomFichier)
 {
-	char octet;
+	TYPE type_paragraphe = GENERATEUR;
+	string ligne;
 	FILE *fichier;
 	
 	fichier = fopen(nomFichier, "rt");
 	if (fichier == NULL)
 		error_fichier_inexistant();
+	
+	while(fgets(ligne, 100, fichier))
+	{
+		if((ligne[0]=='#')||(ligne[0]=='\n')||(ligne[0]=='\r'))
+			continue; // lignes à ignorer, on passe à la suivante
+	}	
 }
--- sim.h
+++ sim.h
@@ -1,6 +1,12 @@
 #ifndef SIM_H
 #define SIM_H
 
+typedef enum type
+{
+	GENERATEUR 	= 1,
+	TROU_NOIR 	= 2,
+	PARTICULE 	= 3
+}	TYPE;
 void sim_lecture(char*);
 
 #endif
