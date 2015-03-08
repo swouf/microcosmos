504f37739b23275e1a388fe3f3ff52d9a3b83b0b
Minh-EPFL <minh1093@gmail.com>
2015-03-08 14:56:12 +0100

    Tentative de d'appliquer l'algorithme sur le fichier trounoir.c mais sans succès car il y a des erreurs de compilation.

--- trounoir.h
+++ trounoir.h
@@ -1,4 +1,10 @@
 #ifndef TROUNOIR_H
 #define TROUNOIR_H
+typedef struct Trounoir
+{
+	float posx;
+	float posy;
+} Trounoir_t;
 
+Trounoir_t** string_parsing_trou_noir(char* lignes[], int nbLignes);
 #endif
