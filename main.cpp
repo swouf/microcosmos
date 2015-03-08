168230ff6077c3de09a2b014c87bd7d5f2784515
swouf <jeremy.jayet@epfl.ch>
2015-03-08 19:00:41 +0100

    Ajout du header au fichier main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
+/*! \file main.cpp
+ * \brief Fichier main du projet
+ * \date
+ * \version 0.1
+ * \author Minh Truong & Jérémy Jayet
+ */
 #include <iostream>
+#include <cstdlib>
 
 extern "C"
 {
@@ -10,8 +17,7 @@ int main(int argc, char **argv)
 {
 	if(argc != 2)
 	{
-		error_msg("Nombre de paramètres invalides.\n \
-					")
+		error_msg("Nombre de paramètres invalides.\n");
 	}
 	
 	
