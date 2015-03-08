4c0092b254ad4b5641f44a3e89fb328ad0b08431
Minh-EPFL <minh1093@gmail.com>
2015-03-08 10:31:03 +0100

    sim_lecture peut désormais lire un fichier texte.

--- sim.c
+++ sim.c
@@ -2,3 +2,13 @@
 #include "particule.h"
 #include "trounoir.h"
 #include "generateur.h"
+
+void sim_lecture(char* nomFichier)
+{
+	char octet;
+	FILE *fichier;
+	
+	fichier = fopen(nomFichier, "rt");
+	if (fichier == NULL)
+		error_fichier_inexistant();
+}
