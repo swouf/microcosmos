3b4d6d4b40e841d0856c0906229fd722c587bd85
swouf <jeremy.jayet@epfl.ch>
2015-03-08 11:59:49 +0100

    Checking nombre arguments fonction main().

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
+#include <iostream>
+
 extern "C"
 {
 	#include "sim.h"
+	#include "error.h"
 }
 
 int main(int argc, char **argv)
 {
+	if(argc != 2)
+	{
+		error_msg("Nombre de paramètres invalides.\n \
+					")
+	}
+	
+	
+	
 	return 0;
 }
