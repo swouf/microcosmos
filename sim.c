f1b58951579beb8a5f5e298d89fdcb921cfe77c3
Minh-EPFL <minh1093@gmail.com>
2015-03-08 11:40:44 +0100

    Écriture de la fonction sim_lecture.

--- sim.c
+++ sim.c
@@ -6,11 +6,15 @@
 #include "trounoir.h"
 #include "generateur.h"
 
+static void lecture_paragraphe(FILE* fichier, int nb_lignes, \
+								int type_paragraphe);
+								
 void sim_lecture(char* nomFichier)
 {
 	TYPE type_paragraphe = GENERATEUR;
 	string ligne;
-	FILE *fichier;
+	FILE *fichier = NULL;
+	int nbLignes = 0;
 	
 	fichier = fopen(nomFichier, "rt");
 	if (fichier == NULL)
@@ -20,5 +24,30 @@ void sim_lecture(char* nomFichier)
 	{
 		if((ligne[0]=='#')||(ligne[0]=='\n')||(ligne[0]=='\r'))
 			continue; // lignes à ignorer, on passe à la suivante
+		else
+		{
+			if(sscanf(ligne, "%u", &nbLignes) != 1)
+			{
+				switch(type_paragraphe)
+				{
+					case GENERATEUR:
+						error_lect_nb_elements(ERR_GENERAT); 
+						break;
+					case TROU_NOIR:
+						error_lect_nb_elements(ERR_TROU_N);
+						break;
+					case PARTICULE:
+						error_lect_nb_elements(ERR_PARTIC);
+				}						
+			}
+			else
+			{
+				lecture_paragraphe(fichier, nb_Lignes, type_paragraphe);
+			}
+		}		
 	}	
+}
+void lecture_paragraphe(FILE* fichier, int nb_lignes, int type_paragraphe)
+{
+	
 }
