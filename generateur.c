aecd4cf3c95590f20a5b4cf63e198fc3d05c9f0d
Minh-EPFL <minh1093@gmail.com>
2015-03-08 13:46:08 +0100

    Implémentation de la fonction string_parsing_generateur.

--- generateur.c
+++ generateur.c
@@ -1,1 +1,18 @@
+#include "stdlib.h"
 #include "generateur.h"
+
+Generateur_t** string_parsing_generateur(char* lignes[], int nbLignes)
+{
+	Generateur_t** tabGenerateurs = malloc(nbLignes*sizeof(Generateur_t*));
+	
+	for(int i = 0; i < nbLignes; i++)
+	{
+		tabGenerateurs[i] = malloc(sizeof(Generateur_t));
+		sscanf(lignes[i], "%f %f %f %f %f",
+				&tabGenerateurs[i]->rgen,
+				&tabGenerateurs[i]->posx,
+				&tabGenerateurs[i]->posy,
+				&tabGenerateurs[i]->vpi_x,
+				&tabGenerateurs[i]->vpi_y);
+	}
+}
