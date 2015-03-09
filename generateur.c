08ebc237bd25f803bb1fdd1d8cd70dce92d60347
Minh-EPFL <minh1093@gmail.com>
2015-03-09 16:08:48 +0100

    générateur.c que j'avais oublié.

--- generateur.c
+++ generateur.c
@@ -1,18 +1,33 @@
-#include "stdlib.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "constantes.h"
+#include "error.h"
 #include "generateur.h"
+
 
 Generateur_t** string_parsing_generateur(char* lignes[], int nbLignes)
 {
 	Generateur_t** tabGenerateurs = malloc(nbLignes*sizeof(Generateur_t*));
 	
+	float rgen, posx, posy, vpi_x, vpi_y, v;
+	v = sqrt((vpi_x*vpi_x)+(vpi_y*vpi_y));
+	
 	for(int i = 0; i < nbLignes; i++)
 	{
 		tabGenerateurs[i] = malloc(sizeof(Generateur_t));
 		sscanf(lignes[i], "%f %f %f %f %f",
-				&tabGenerateurs[i]->rgen,
-				&tabGenerateurs[i]->posx,
-				&tabGenerateurs[i]->posy,
-				&tabGenerateurs[i]->vpi_x,
-				&tabGenerateurs[i]->vpi_y);
+				&rgen,
+				&posx,
+				&posy,
+				&vpi_x,
+				&vpi_y);
+		
+		if((rgen < RMIN) || (rgen > RMAX))
+			error_rayon_partic(ERR_GENERAT, rgen);
+		
+		if(v > MAX_VITESSE)
+			error_vitesse_partic(ERR_GENERAT, v);
 	}
+	return tabGenerateurs;
 }
