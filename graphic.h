47fb5c16fd8904b481dbd5f80c857b39723f275e
swouf <jeremy.jayet@epfl.ch>
2015-04-05 12:35:46 +0200

    Ajout du fichier d'interface graphic.h

---
+++ graphic.h
@@ -0,0 +1,12 @@
+/*!
+ * \file graphic.h
+ * \brief (HEADER) Module de dessin
+ * \date 15.03.2015
+ * \version 1
+ * \author Minh Truong & Jérémy Jayet
+ */
+
+#ifndef GRAPHIC_H
+#define GRAPHIC_H
+
+#endif
