cdf769b51e3478e679a7f513c477781e1aabf220
Minh-EPFL <minh1093@gmail.com>
2015-04-19 16:42:57 +0200

    Documentation des fonctions de generateur.h

--- generateur.h
+++ generateur.h
@@ -21,16 +21,115 @@ typedef struct Generateur Generateur_t;
  */
 Generateur_t* string_parsing_generateur(char*);
 
+/*!
+ * \fn void display_generateurs(void)
+ * \brief Affiche les générateurs
+ */
 void display_generateurs(void);
 
+/*!
+ * \fn void clean_generateurs(void)
+ * \brief Vide complètement la liste chaînée contenue dans le module et
+ *          remets le pointeur ptrGenerateurs ainsi que la variable
+ *          nbGenerateurs respectivement à NULL et à 0 (zéro).
+ */
 void clean_generateurs(void);
 
+/*!
+ * \fn Generateur_t* get_gen_by_id(int id)
+ * \brief Récupère un pointeur sur un générateur en donnant son id (sa
+ *          place dans la liste contenant les générateurs).
+ * \param id : place du générateur dans la liste contenant les 
+ * 				générateurs.
+ * \return Pointeur sur le générateur, ou NULL en cas d'erreur (id trop
+ *          grand ou autre)
+ */
 Generateur_t* get_gen_by_id(int);
+
+/*!
+ * \fn get_gen_rgen(Generateur_t* gen)
+ * \brief Récupére le rayon d'un générateur.
+ * \param gen: pointeur sur le générateur dont on veut récupérer
+ *             le rayon.
+ * \return Rayon de la particule générée.
+ */
 double get_gen_rgen(Generateur_t*);
+
+/*!
+ * \fn double get_gen_posx(Generateur_t* gen)
+ * \brief Récupére la position en x d'un générateur.
+ * \param gen : pointeur sur le générateur dont on veut récupérer
+ *                   la position x.
+ * \return Position en x du générateur.
+ */
 double get_gen_posx(Generateur_t*);
+
+/*!
+ * \fn double get_gen_posy(Generateur_t* gen)
+ * \brief Récupére la position en y d'un générateur.
+ * \param gen : pointeur sur le générateur dont on veut récupérer
+ *                   la position y.
+ * \return Position en y du générateur.
+ */
 double get_gen_posy(Generateur_t*);
+
+/*!
+ * \fn get_gen_vpix(Generateur_t* gen)
+ * \brief Récupère la vitesse initiale x de la particule générée.
+ * \param gen : pointeur sur le générateur dont on veut récupérer
+ *                   la vitesse initiale x de la particule.
+ * \return La vitesse initiale x de la particule générée.
+ */
 double get_gen_vpix(Generateur_t*);
+
+/*!
+ * \fn get_gen_vpiy(Generateur_t* gen)
+ * \brief Récupère la vitesse initiale y de la particule générée
+ * \param gen : pointeur sur le générateur dont on veut récupérer
+ *                   la vitesse initiale y de la particule.
+ * \return La vitesse initiale y de la particule générée.
+ */
 double get_gen_vpiy(Generateur_t*);
+
+/*!
+ * \fn get_nb_generateurs(void)
+ * \brief Récupère le nombre de générateurs.
+ * \return Le nombre de générateurs.
+ */
 int get_nb_generateurs(void);
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
